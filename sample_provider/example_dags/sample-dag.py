55d047ec4e71a5a7d35608f7f4ecc2e8b0aa6e5a
Josh Fell <josh.d.fell@astronomer.io>
2022-04-11 12:10:24 -0400

    Update DAG with a few best practices

--- sample_provider/example_dags/sample-dag.py
+++ sample_provider/example_dags/sample-dag.py
@@ -1,44 +1,36 @@
+from pendulum import datetime
 from datetime import timedelta
-import json
 
 from airflow.decorators import dag, task
-from airflow.utils.dates import days_ago
 
 from sample_provider.operators.sample_operator import SampleOperator
 from sample_provider.sensors.sample_sensor import SampleSensor
 
 
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    'owner': 'airflow',
-}
-
-
-@dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['example'])
-def sample_workflow():
+@dag(
+    start_date=datetime(2022, 1, 1),
+    schedule_interval=None,
+    # ``default_args`` will get passed on to each task. You can override them on a per-task basis during
+    # operator initialization.
+    default_args={"retries": 2, sample_conn_id: "conn_sample"},
+    tags=["example"],
+    default_view="graph",
+)
+def sample_worflow():
     """
     ### Sample DAG
 
     Showcases the sample provider package's operator and sensor.
 
-    To run this example, create a connector with:
+    To run this example, create an HTTP connection with:
     - id: conn_sample
     - type: http
-    - host: www.httpbin.org	
+    - host: www.httpbin.org
     """
 
-    task_get_op = SampleOperator(
-        task_id='get_op',
-        sample_conn_id='conn_sample',
-        method='get',
-    )
+    task_get_op = SampleOperator(task_id="get_op", method="get")
 
-    task_sensor = SampleSensor(
-        task_id='sensor',
-        sample_conn_id='conn_sample',
-        endpoint=''
-    )
+    task_sensor = SampleSensor(task_id="sensor", sample_conn_id="conn_sample", endpoint="")
 
     task_get_op >> task_sensor
 
